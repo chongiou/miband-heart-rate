891980c92644cf01a84d3d7794b884da2d1562d9
重楼 <chongiou@qq.com>
2025-02-28 17:59:32 +0800

    feat: 改为json输出, 并添加选项 server <http url> 用于提交数据

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,77 @@
-use std::error::Error;
-
 use bluest::{Adapter, AdvertisingDevice};
+use chrono::Utc;
+use clap::Parser;
 use futures_lite::stream::StreamExt;
+use serde_json;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let adapter = Adapter::default()
-        .await
-        .ok_or("Bluetooth adapter not found")?;
-    adapter.wait_available().await?;
-
-    println!("Starting scan");
-    let mut scan = adapter.scan(&[]).await?;
-
-    println!("Scan started");
-    while let Some(discovered_device) = scan.next().await {
-        handle_device(discovered_device)
-    }
-    Ok(())
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    server: Option<String>,
 }
 
-fn handle_device(discovered_device: AdvertisingDevice) {
-    if let Some(manufacturer_data) = discovered_device.adv_data.manufacturer_data {
-        if manufacturer_data.company_id != 0x0157 {
-            return;
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let adapter = Adapter::default()
+        .await
+        .ok_or("Bluetooth adapter not found")
+        .unwrap();
+    adapter.wait_available().await.unwrap();
+    let client = reqwest::Client::new();
+
+    println!("Starting scan Xiaomi Band");
+    let mut scan = adapter.scan(&[]).await.unwrap();
+    while let Some(discovered_device) = scan.next().await {
+        if let Some(json_output) = handle_device(discovered_device) {
+            println!("{}", json_output);
+
+            if args.server != None {
+                let response = client
+                    .post(&format!("{:?}", args.server))
+                    .json(&json_output)
+                    .send()
+                    .await
+                    .unwrap()
+                    .json::<serde_json::Value>()
+                    .await;
+                
+                match response {
+                    Ok(_res) => {
+                    }
+                    Err(err) => {
+                        eprintln!("Request Error: {:#?}", err)
+                    }
+                }
+            }
         }
-        let name = discovered_device
-            .device
-            .name()
-            .unwrap_or(String::from("(unknown)"));
-        let rssi = discovered_device.rssi.unwrap_or_default();
-        let heart_rate = manufacturer_data.data[3];
-        println!("{name} ({rssi}dBm) Heart Rate: {heart_rate:?}",);
     }
+}
+
+fn handle_device(discovered_device: AdvertisingDevice) -> Option<serde_json::Value> {
+    let manufacturer_data = discovered_device.adv_data.manufacturer_data?;
+    if manufacturer_data.company_id != 0x0157 {
+        return None;
+    }
+    let name = discovered_device
+        .device
+        .name()
+        .unwrap_or(String::from("(unknown)"));
+    let id = discovered_device.device.id();
+    let rssi = discovered_device.rssi.unwrap_or_default(); //dBm
+
+    let heart_rate: Option<u8> = match manufacturer_data.data[3] {
+        0xFF => None,
+        x => Some(x.into()),
+    };
+    let json_output = serde_json::json!({
+        "timestamp": Utc::now().timestamp(),
+        "name": name,
+        "id": id.to_string(),
+        "rssi": rssi,
+        "heart_rate": heart_rate
+    });
+    Some(json_output)
 }
